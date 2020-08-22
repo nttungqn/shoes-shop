b14a08223bf0b8eacef75f354bba2a7d6149a2ad
nttungqn <1712882@student.hcmus.edu.vn>
2020-08-22 09:27:04 +0700

    add config.env

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /** @format */
 
-const express = require("express");
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = express();
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
