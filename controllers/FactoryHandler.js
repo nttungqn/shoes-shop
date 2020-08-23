d46dbd1dd97f3c6e9ee803d7a366a801753fcc63
nttungqn <1712882@student.hcmus.edu.vn>
2020-08-23 09:57:20 +0700

    + utils/catchAsyc.js

--- controllers/FactoryHandler.js
+++ controllers/FactoryHandler.js
@@ -1,124 +1,126 @@
 /** @format */
+const catchAsync = require('./../utils/catchAsync');
 
-module.exports.getAll = (Model) => async (req, res, next) => {
-	try {
-		const docs = await Model.find({});
-		res.status(200).json({
-			status: 'success',
-			data: {
-				length: docs.length,
-				docs,
-			},
-		});
-	} catch (err) {
-		res.status(404).json({
-			status: 'error',
-			message: err,
-		});
-	}
-
-	next();
-};
-
-module.exports.createOne = (Model) => async (req, res, next) => {
-	try {
-		const doc = await Model.create(req.body);
-		res.status(201).json({
-			status: 'success',
-			data: {
-				doc,
-			},
-		});
-	} catch (err) {
-		res.status(404).json({
-			status: 'error',
-			message: err,
-		});
-	}
-
-	next();
-};
-
-module.exports.getOne = (Model) => async (req, res, next) => {
-	try {
-		const doc = await Model.findById({ _id: req.params.id });
-
-		if (!doc) {
-			res.status(204).json({
-				status: 'error',
+module.exports.getAll = (Model) =>
+	catchAsync(async (req, res, next) => {
+		try {
+			const docs = await Model.find({});
+			res.status(200).json({
+				status: 'success',
 				data: {
-					status: 'fail',
-					message: `No document with that id = ${req.params.id}`,
+					length: docs.length,
+					docs,
 				},
 			});
-		} else {
-			res.status(200).json({
+		} catch (err) {
+			res.status(404).json({
+				status: 'error',
+				message: err,
+			});
+		}
+	});
+
+module.exports.createOne = (Model) =>
+	catchAsync(async (req, res, next) => {
+		try {
+			const doc = await Model.create(req.body);
+			res.status(201).json({
 				status: 'success',
 				data: {
 					doc,
 				},
 			});
-		}
-	} catch (err) {
-		res.status(404).json({
-			status: 'error',
-			message: err,
-		});
-	}
-};
-
-module.exports.updateOne = (Model) => async (req, res, next) => {
-	try {
-		const doc = await Model.findByIdAndUpdate(req.params.id, req.body);
-
-		if (!doc) {
-			res.status(204).json({
+		} catch (err) {
+			res.status(404).json({
 				status: 'error',
-				data: {
-					status: 'fail',
-					message: `No document with that id = ${req.params.id}`,
-				},
-			});
-		} else {
-			res.status(200).json({
-				status: 'success',
-				data: {
-					doc,
-				},
+				message: err,
 			});
 		}
-	} catch (err) {
-		res.status(404).json({
-			status: 'error',
-			message: err,
-		});
-	}
-};
+	});
 
-module.exports.deleteOne = (Model) => async (req, res, next) => {
-	try {
-		const doc = await Model.findOneAndDelete(req.params.id, req.body);
+module.exports.getOne = (Model) =>
+	catchAsync(async (req, res, next) => {
+		try {
+			const doc = await Model.findById({ _id: req.params.id });
 
-		if (!doc) {
-			res.status(204).json({
+			if (!doc) {
+				res.status(204).json({
+					status: 'error',
+					data: {
+						status: 'fail',
+						message: `No document with that id = ${req.params.id}`,
+					},
+				});
+			} else {
+				res.status(200).json({
+					status: 'success',
+					data: {
+						doc,
+					},
+				});
+			}
+		} catch (err) {
+			res.status(404).json({
 				status: 'error',
-				data: {
-					status: 'fail',
-					message: `No document with that id = ${req.params.id}`,
-				},
-			});
-		} else {
-			res.status(200).json({
-				status: 'success',
-				data: {
-					doc,
-				},
+				message: err,
 			});
 		}
-	} catch (err) {
-		res.status(404).json({
-			status: 'error',
-			message: err,
-		});
-	}
-};
+	});
+
+module.exports.updateOne = (Model) =>
+	catchAsync(async (req, res, next) => {
+		try {
+			const doc = await Model.findByIdAndUpdate(req.params.id, req.body);
+
+			if (!doc) {
+				res.status(204).json({
+					status: 'error',
+					data: {
+						status: 'fail',
+						message: `No document with that id = ${req.params.id}`,
+					},
+				});
+			} else {
+				res.status(200).json({
+					status: 'success',
+					data: {
+						doc,
+					},
+				});
+			}
+		} catch (err) {
+			res.status(404).json({
+				status: 'error',
+				message: err,
+			});
+		}
+	});
+
+module.exports.deleteOne = (Model) =>
+	catchAsync(async (req, res, next) => {
+		try {
+			const doc = await Model.findOneAndDelete(req.params.id, req.body);
+
+			if (!doc) {
+				res.status(204).json({
+					status: 'error',
+					data: {
+						status: 'fail',
+						message: `No document with that id = ${req.params.id}`,
+					},
+				});
+			} else {
+				res.status(200).json({
+					status: 'success',
+					data: {
+						doc,
+					},
+				});
+			}
+		} catch (err) {
+			res.status(404).json({
+				status: 'error',
+				message: err,
+			});
+		}
+	});
