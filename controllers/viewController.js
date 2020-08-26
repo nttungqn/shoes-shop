cf755d1244f788080a24fa4310f629cce609b018
nttungqn <1712882@student.hcmus.edu.vn>
2020-08-26 10:10:02 +0700

    delete template

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,6 +1,7 @@
 /** @format */
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/AppError');
+const Product = require('./../models/productModel');
 
 // 1) Get tour data from collection
 // 2) Build template
@@ -13,8 +14,11 @@ module.exports.getOverview = catchAsync(async (req, res, next) => {
 });
 
 module.exports.getShopCategory = catchAsync(async (req, res, next) => {
+	const products = await Product.find();
+
 	res.status(200).render('category', {
 		title: 'Shop Category',
+		products,
 	});
 });
 
