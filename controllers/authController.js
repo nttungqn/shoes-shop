8c246366840dbdee104b27534d1cc709a6512af9
nttungqn <1712882@student.hcmus.edu.vn>
2020-08-27 11:09:22 +0700

    xac thu login

---
+++ controllers/authController.js
@@ -0,0 +1,50 @@
+/** @format */
+
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const createResponse = (user, statusCode, req, res) => {
+	// Remove password from output
+	user.password = undefined;
+
+	res.status(statusCode).json({
+		status: 'success',
+		data: {
+			user,
+		},
+	});
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+	const newUser = await User.create({
+		name: req.body.name,
+		email: req.body.email,
+		password: req.body.password,
+		passwordConfirm: req.body.passwordConfirm,
+	});
+
+	createResponse(newUser, 201, req, res);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+	const { email, password } = req.body;
+
+	// 1) Check if email and password exist
+	if (!email || !password) {
+		return next(new AppError('Please provide email and password!', 400));
+	}
+	// 2) Check if user exists && password is correct
+	const user = await User.findOne({ email }).select('+password');
+
+	if (!user || !(await user.correctPassword(password, user.password))) {
+		return next(new AppError('Incorrect email or password', 401));
+	}
+
+	// 3) If everything ok, send token to client
+	createResponse(user, 200, req, res);
+});
+
+exports.logout = (req, res) => {
+	res.status(200).json({ status: 'success' });
+};
--- models/userModel.js
+++ models/userModel.js
@@ -30,9 +30,6 @@ const userSchema = new mongoose.Schema({
 			message: 'Passwords are not the same',
 		},
 	},
-	passwordChangedAt: Date,
-	passwordResetToken: String,
-	passwordResetExpires: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -56,29 +53,6 @@ userSchema.pre('save', function (next) {
 
 userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
 	return await bcrypt.compare(candidatePassword, userPassword);
-};
-
-userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
-	if (this.passwordChangedAt) {
-		const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
-
-		return JWTTimestamp < changedTimestamp;
-	}
-
-	// False means NOT changed
-	return false;
-};
-
-userSchema.methods.createPasswordResetToken = function () {
-	const resetToken = crypto.randomBytes(32).toString('hex');
-
-	this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
-
-	// console.log({ resetToken }, this.passwordResetToken);
-
-	this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
-
-	return resetToken;
 };
 
 const User = mongoose.model('User', userSchema, 'users');
